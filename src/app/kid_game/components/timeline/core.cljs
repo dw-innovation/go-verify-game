a2a43390586eaf57cb702c567a419bdc4045f9a7
Basile Simon <basilesimon@users.noreply.github.com>
2021-09-22 10:51:08 +0200

    Style/post (#18)
    * Style and refactor <author>
    
    * Style and refactor post copy, media
    
    * Prettier run, ignore scss
    
    * Return to two-columns layout, a la Twitter
    
    * Extract post actions
    
    * Extract post comments
    
    * Style, progress bar

--- src/app/kid_game/components/timeline/core.cljs
+++ src/app/kid_game/components/timeline/core.cljs
@@ -1,16 +1,16 @@
 (ns kid-game.components.timeline.core
-  (:require [reagent.core :as r]
-            [kid-game.state :as state]
-            [kid-game.business :as business]
-            [kid-game.utils.log :as log]
-            [kid-shared.types.post :as posts]
+  (:require [cljs.js                  :refer [empty-state eval js-eval]]
+            [cljs.core.async          :as async :include-macros true]
+            [reagent.core             :as r]
+            [kid-game.state           :as state]
+            [kid-game.business        :as business]
+            [kid-game.utils.log       :as log]
+            [kid-shared.types.post    :as posts]
             [kid-shared.types.comment :as comment]
-            [kid-game.utils.core :refer [timestamp-now new-uuid]]
-            [lodash]
-            [moment]
+            [kid-game.utils.core      :refer [timestamp-now new-uuid]]
             [react-transition-group]
-            [cljs.js :refer [empty-state eval js-eval]]
-            [cljs.core.async :as async :include-macros true]))
+            [lodash]
+            [moment]))
 
 
 ;; documentation for css transition group seems kind of tricky but is here:
@@ -21,42 +21,61 @@
 (def css-transition
   (r/adapt-react-class react-transition-group/CSSTransition))
 
-
 (declare <post>)
 
 (defn <progress> [amnt total]
   (let [percent (* 100 (/ amnt total))
         percent-left (- 100 percent)]
-  ;(log/debug (str (* 100 (/ amnt total)) "%"))
-  [:div {:class "progress-bar"}
-   [:div {:class "progress-inner"
-          :percent percent
-          :style {:width (str percent-left  "%")
-                  }}]]))
+    [:progress {:class "progress is-primary is-small"
+                :value percent
+                :max 100}]))
 
 (defn <post-progress> [{time-left :time-left
                         time-limit :time-limit}]
   [<progress> time-left time-limit])
 
+(defn <debug-tags> [p]
+  [:div.tags
+   [:div {:class "tag is-light is-info is-family-monospace"} (:id p)]
+   [:div {:class "tag is-light is-info is-family-monospace"} (:game-state p)]])
 
 (defn <author-image> [{:as author
                        image :image
                        name :name}]
-   [:div {:class "post-author-image"}
-    (when image [:img {:src image
-                       :alt (str name " profile image")}])])
+  [:div.author-image (when image [:img {:src image
+                                        :alt (str name " profile image")}])])
 
 (defn <author-name> [{:as author name :name
                       handle :handle}]
-  [:div {:class "post-author-name"}
-   [:span.name name] [:span.handle handle]])
+  [:div.level.mb-1
+   [:div.level-left
+    [:div.level-item [:h6 {:class "title is-6 m-0"}     name]]
+    [:div.level-item [:p  {:class "has-text-grey ml-0"} handle]]]])
 
-(defn <author> [{:as author
-                 name :name
-                 image :image}]
-  [:div {:class "post-author"}
-   [<author-image> author]
-   [<author-name> author]])
+(defn <post-text> [copy]
+  [:div.block.mb-3
+   [:p copy]])
+
+(defn <post-media> [img]
+  [:div.block
+   [:figure {:class "image is-16by9"}
+    [:img {:class "post-media" :src img}]]])
+
+(defn <post-actions> [p]
+  (let [investigate! (fn [] (business/post-investigate! p))
+        block! (fn [] (business/post-block! p))
+        share! (fn [] (business/post-share! :comment "comment about post"
+                                            :post p))]
+    [:div.buttons
+     [:button {:class "button outline" :on-click share!}
+      [:span.icon [:i {:class "fas fa-share"}]] [:span "share"]]
+     [:button {:class "button outline" :on-click block!}
+      [:span.icon [:i {:class "fas fa-ban"}]] [:span "block"]]
+     [:button {:class "button outline" :on-click (fn [ev] ;; stop propagation because there is a global
+                                        ;; click to open panel, and we are specifically opening the other one
+                                                   (.stopPropagation ev)
+                                                   (investigate!))}
+      [:span.icon [:i {:class "fas fa-search"}]] [:span "investigate"]]]))
 
 ;; will center the elements in a post overlay
 (defn <post-overlay> [{:as p
@@ -85,9 +104,7 @@
       nil)
     (when points-result (let [lost? (neg? points-result)
                               points (js/Math.abs points-result)]
-                          [:div "You " (if lost? "lost " "won ") points " points"]))
-
-    ]])
+                          [:div "You " (if lost? "lost " "won ") points " points"]))]])
 
 (defn <comment> [{:as comment
                   text :text
@@ -100,78 +117,60 @@
      [:div.comment-author (:name author) [:span.comment-handle (:handle author)]]
      [:div.comment-text text]]]])
 
+(defn <post-comments> [comments]
+  [css-transition-group {:class "post-comments"}
+   (for [comment comments]
+     [css-transition {:timeout     2000
+                      :key         (comment/id comment)
+                      :class-names "post-transition"}
+      ^{:key (comment/id comment)} ;; important to keep track of rendering
+      [<comment> comment]])])
+
 (defn <type-text> [;; destructure the post
                    {:as p
                     title :title
                     description :description
-                    ;; fake-news? either true or false
-                    fake-news? :fake-news?
-                    ;; game-state is either :live, :shared, :blocked, or :timed-out
-                    game-state :game-state
-                    ;; destructure the author:
-                    {:as author author-name :name} :by
+                    fake-news? :fake-news? ; either true or false
+                    game-state :game-state ; either :live, :shared, :blocked, or :timed-out
+                    {:as author author-name :name} :by ; destructure the author
                     comments :comments
                     time-limit :time-limit}]
-  (let [investigate! (fn [] (business/post-investigate! p))
-        block! (fn [] (business/post-block! p))
-        share! (fn [] (business/post-share! :comment "comment about post"
-                                            :post p))]
-    [:div {:class ["post" "post-type-text" game-state]}
-     [:div.testing-purposes (:id p)]
-     [:small.testing-purposes (:game-state p)]
-     [:div.post-inner
-      [:div.post-left-column
-       [<author-image> author]]
-      [:div.post-right-column
-       [<author-name> author]
-       [:div {:class "post-text"}
-        [:div.post-description description]]
-       (when (:image p) [:img.post-image {:src (:image p)}])
-       (when (= game-state :live)
-         [:div {:class "post-actions"}
-          [:button {:on-click share!} "share"]
-          [:button {:on-click block!} "block"]
-          [:button {:on-click (fn [ev] ;; stop propagation because there is a global
-                                ;; click to open panel, and we are specifically opening the other one
-                                (.stopPropagation ev)
-                                (investigate!))}
-           "investigate"]])
-       (when (= game-state :live) [<post-progress> p])
-
-       [css-transition-group {:class "post-comments"}
-        (for [comment comments]
-          [css-transition {:timeout 2000
-                           :key (comment/id comment)
-                           :class-names "post-transition"}
-           ^{:key (comment/id comment)} ;; important to keep track of rendering
-           [<comment> comment]])]
-       ]
-      (case game-state
-        :live nil
-        :shared [<post-overlay> p]
-        :blocked [<post-overlay> p]
-        :timed-out [<post-overlay> p]
-        nil)
-     ]]))
+  [:div {:class ["post" "post-type-text my-5" game-state]}
+   [<debug-tags> p]
+   [:div.columns.mr-0
+    [:div {:class "column is-1 p-0"} [<author-image> author]]
+    [:div {:class "column is-11 p-0 pl-3"}
+     [<author-name> author]
+     [<post-text> description]
+     (when (:image p) [<post-media> (:image p)])
+     (when (= game-state :live) [<post-actions> p])
+     (when (= game-state :live) [<post-progress> p])
+     [<post-comments> comments]
+     (case game-state
+       :live      nil
+       :shared    [<post-overlay> p]
+       :blocked   [<post-overlay> p]
+       :timed-out [<post-overlay> p]
+       nil)]]])
 
 (defn <type-re-post> [{:as p
-                author :by
-                comment :comment
-                original-post :post}]
+                       author :by
+                       comment :comment
+                       original-post :post}]
   [:div {:class ["post" "post-type-re-post"]}
-   [:div.post-inner
-    [:div.post-left-column
-     [<author-image> author]]
-    [:div.post-right-column
+   [<debug-tags> p]
+   [:div.columns.mr-0
+    [:div {:class "column is-1 p-0"} [<author-image> author]]
+    [:div {:class "column is-11 p-0 pl-3"}
      [<author-name> author]
-     [:div {:class "post-description"} comment]
+     [<post-text> comment]
      [:div {:class "post-sub-post"} (<post> original-post)]]]])
 
 (defn match-post [p]
   (case (:type p)
     :post-default [<type-text> p]
-    :post-text [<type-text> p]
-    :re-post [<type-re-post> p]
+    :post-text    [<type-text> p]
+    :re-post      [<type-re-post> p]
     ; post type not found:
     (do
       (log/warn "no matching component for post type" (str p))
@@ -180,10 +179,10 @@
 
 (defn <post> [p]
   ^{:key (:id p)} ;; important to keep track of rendering
-   [match-post p])
+  [match-post p])
 
 (defn <header> []
-  [:div {:class ["panel-header" "timeline-header" ]}
+  [:div {:class ["panel-header" "timeline-header"]}
    [:h5 {:class "title is-5 is-white"} "Your timeline"]])
 
 (defn <container> []
@@ -197,6 +196,5 @@
                                     :key (:id post)
                                     :class-names "post-transition"}
                     ^{:key (:id post)} ;; important to keep track of rendering
-                    [:div.post-in-list
-                     [<post> post]]])
+                    [<post> post]])
                  (state/posts))]])
