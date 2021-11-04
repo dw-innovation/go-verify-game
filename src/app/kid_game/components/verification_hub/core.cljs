43d779c855fc09fe2af0e66d071b8beefdda01b9
Basile Simon <basile@basilesimon.fr>
2021-11-04 14:58:36 +0100

    General layout

--- src/app/kid_game/components/verification_hub/core.cljs
+++ src/app/kid_game/components/verification_hub/core.cljs
@@ -29,28 +29,24 @@
 
 (defn <thomas> [] [:div.thomas.columns.is-centered
                    [:div.column.is-3
-                   [:div.thomas-icon
-                    [icons/thomas]]]])
+                    [:div.thomas-icon
+                     [icons/thomas]]]])
 
 (defn <hub-icon> [{icon :icon
                    title :title
                    on-click! :fn}]
-  [:a.hub-icon
-   {:on-click on-click!}
+  [:a.hub-icon {:on-click on-click!}
    [icon]
    [:div.has-text-centered.has-text-black title]])
 
 (defn <hub-icons> [[{icon :icon
-                    title :title
+                     title :title
                      on-click! :fn} & rest :as actions]]
-  [:div.icons.columns.mt-5
-   (map <hub-icon> actions)])
-
-(defn <hub-icons-vertical> [[{icon :icon
-                              title :title
-                              on-click! :fn} & rest :as actions]]
-  [:div.icons.rows
-   (map <hub-icon> actions)])
+  [:div.has-text-centered
+   [:h4.title.is-4 "Thomas recommends you try one of the following"]
+   [:p.subtitle "Click on one of the options to start the corresponding activity"]
+   [:div.icons.columns.mt-5
+   (map <hub-icon> actions)]])
 
 (defn <hub-home> [{post :post
                    change-panel! :change-panel!}]
@@ -75,10 +71,10 @@
                   :title "Web Search"
                   :fn (choose-activity! :web-search)}
                  {:icon icons/recycle-search
-                  :title "Reverse Image Search"
+                  :title "Reverse image search"
                   :fn (choose-activity! :reverse-image-crop)}
                 {:icon icons/image-analysis
-                  :title "Image-analysis"
+                  :title "Image analysis"
                  :fn (choose-activity! :polygon-search)}
                 {:icon icons/geolocation
                   :title "Geolocation"
@@ -104,9 +100,6 @@
        [icons/hourglass]
        [:div missed-deadlines " Missed Deadlines"]]]]))
 
-
-
-
 (defn <container> []
   (let [active-panel (r/atom :hub) ; or activity type such as :reverse-image-crop
         change-panel! (fn [activity-type] (reset! active-panel activity-type))
@@ -116,15 +109,9 @@
             points @state/points]
         [:div
          [<header>]
-
-            [:div {:class "notification is-info is-light"}
-             [:b "Currently investigating: "] "post ID #" (:id post)]
-
-
          [:div.hub-container
             (case @active-panel
               :hub [<hub-home> {:post post :change-panel! change-panel!}]
               [<investigate-post> {:post post
                                    :activity-type @active-panel
-                                   :back! back-to-hub!}])
-            ]]))))
+                                   :back! back-to-hub!}])]]))))
