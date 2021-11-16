633ee9ccc8a8e08df53a91c0094a8908bb6fb91a
niko <niko@niko.io>
2021-11-16 11:53:05 +0100

    Added :only flag to debugger

--- src/app/kid_game/utils/log.clj
+++ src/app/kid_game/utils/log.clj
@@ -2,8 +2,23 @@
   (:require [kid-shared.types.shared :as shared]
             [clojure.spec.alpha :as s]))
 
+
+;;
+;;  debug macro
+;;   use like: (log/debug "handling message" msg)
+;;
+;;  this will result in something like "[kid-game.business] handling message {}"
+;;   - the namespace has automatically been added
+;;
+;;   if you want to mute all other debug calls, then you can pass the :only flag like:
+;;     (log/debug :only "handling message" msg)
+;;     - this will mute all other prints except those with the only flag
+;;
 (defmacro debug [& args]
   `(do
+     ;; set the print-all? atom to false, if :only has been passed as a flag
+     (when (some #(= :only %) [~@args]) (reset! log/print-all? false))
      ;; in case we need more here
      ;; TODO only print if dev
-     (println (str "[" ~(str (:name (:ns &env))) "]") "\n" ~@args)))
+     (when (or (some #(= :only %) [~@args]) @log/print-all?)
+              (println (str "[" ~(str (:name (:ns &env))) "]") "\n" ~@args))))
--- src/app/kid_game/utils/log.cljs
+++ src/app/kid_game/utils/log.cljs
@@ -3,5 +3,7 @@
   (:require [kid-shared.types.shared :as shared]
             [clojure.spec.alpha :as s]))
 
+(def print-all? (atom true))
+
 (def warn println)
 (def error js/console.error)
