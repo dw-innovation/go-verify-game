7f7600af0e0c3a879f780536282dd39ee267b1eb
niko <niko@niko.io>
2021-11-19 12:22:54 +0100

    Style and cleanup

--- src/app/kid_game/components/verification_hub/activities/ris_flip.cljs
+++ src/app/kid_game/components/verification_hub/activities/ris_flip.cljs
@@ -16,15 +16,13 @@
 (= true (atom? (atom 3)))
 (= true (atom? (r/atom 3)))
 
+;; TODO write function spec to check this pred
 (defn is-step-function?
   "to be a step function, it must return [atom, component]"
   [ret] (let [[status <component>] ret]
           (and (atom? status)
                (status #{:strted :failed :succeeded})
                (fn? <component>))))
-
-(defn empty-step []
-  [(r/atom :idle) (fn [] [:<> "empty"])])
 
 ;; FIXME: in some cases, this NS must be explicitly imported up the
 ;; requirement tree for changed to be picked up. no idea why. see issue #48
@@ -38,8 +36,7 @@
   (r/adapt-react-class react-transition-group/CSSTransition))
 
 
-(defn <drag-step> [{img-src :img-src
-                    search-results :search-results}]
+(defn drag-step []
   "returns a vector of type [atom, reagent-function] where atom is the status of the step"
   (let [status (r/atom :started)
         dragged? (r/atom false)
@@ -49,8 +46,12 @@
                      (async/go (async/<! (async/timeout 2000))
                                (reset! dragged? true)
                                (reset! loading? false)
+                               (async/<! (async/timeout 1000))
+                               (reset! status :succeeded)
                                ))]
-    (fn []
+    [status
+    (fn [{img-src :img-src
+          search-results :search-results}]
       [:div
        [image-results/<dragger> img-src drag-done!]
        [css-transition-group {:class "transition-results" :timeout 100}
@@ -62,32 +63,47 @@
               [:h4.title.is-4.mb-2 "Results from your image search"]
               [:h3.ris-result-header [:b (count search-results)] " page(s) with matching images"]
               [image-results/<search-results> search-results]
-              [:hr]]]))]])))
+              [:hr]]]))]])]))
 
-(defn flip-step [{img-src :img-src text :text} ]
+(defn flip-step []
   "returns a vector of type [atom, reagent-function] where atom is the status of the step"
-  (let [status (r/atom :started)]
+  (let [status (r/atom :started)
+        flip! (fn []
+                (reset! status :flipping)
+                (async/go
+                  (async/<! (async/timeout 2000)) ;sync this timout with animation length on line +2
+                  (reset! status :succeeded)))
+        animation-css {:animation "flip-animate 2s ease-in-out"
+                       :animation-fill-mode "forwards"}]
     [status
-     (fn []
-       [:<>
-        [:hr]
-        text
-        [:button {:on-click (fn [] (reset! status :succeeded) (println "won"))} "win me"]
-        [:button {:on-click (fn [] (reset! status :failed) (println "failed"))} "fail me"]
-        ])]))
+     (fn [{img-src :img-src}]
+        [:div {:class "is-flex is-justify-content-center mb-5"}
+         [:div
+          [:div
+           [:h3.title.is-3 "No Results?"]
+           [:p.subtitle "Try flipping the image and searching again"]
+           ]
+          [:div.mt-3 {:style (when (#{:flipping :succeeded} @status) animation-css)}
+           [:img {:src img-src}]]
+          [:div
+           [:button {:on-click flip!} "Flip!"]]
+          ]]
+        )]))
 
 
-(defn <main> [{} back!]
-  (let [[s1 <s1>] (flip-step {:text "first-step"})]
+(defn <main> [{:as data
+               main-image :main-image
+               flipped-image :flipped-image
+               search-results :result-search} back!]
+  (let [[s1 <s1>] (drag-step)
+        [s2 <s2>] (flip-step)
+        [s3 <s3>] (drag-step)]
     (fn []
-      (let [[s2 <s2>] (do  (case @s1 :succeeded (flip-step {:text "you did it!"})
-                                 (empty-step)))]
-        [(fn []
-           (let [[s3 <s3>] (flip-step {:text (case @s2 :succeeded "second task done"
-                                                   :failed "second task failed"
-                                                   "")})]
-             [(fn []
-                [:div
-                 [<s1>]
-                 [<s2>]
-                 [<s3>]])]))]))))
+      [:div.activity-container
+       [:div.activity-step.contain-section-width.center-section
+        [<s1> {:img-src main-image :search-results []}]]
+       [:div.activity-step.contain-section-width.center-section
+        (when (= @s1 :succeeded) [<s2> {:img-src main-image}])]
+       [:div.activity-step.contain-section-width.center-section
+        (when (#{:succeeded} @s2) [<s3> {:img-src flipped-image
+                                         :search-results search-results}])]])))
