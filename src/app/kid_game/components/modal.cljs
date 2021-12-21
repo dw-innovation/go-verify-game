37353870df07bd696defd2cd0b7906825b7aca2b
niko <niko@niko.io>
2021-12-21 17:19:48 +0100

    further formatting

--- src/app/kid_game/components/modal.cljs
+++ src/app/kid_game/components/modal.cljs
@@ -36,41 +36,40 @@
   (fn []
     (let [esc-listener (setup-esc-listener)]   ;; which ends up being an unused binding...
       [:div {:class "modal"
-             :id "modal"}
-       [:div {:class "modal-background"
+             :id    "modal"}
+       [:div {:class    "modal-background"
               :on-click #(close-modal)}]
        [:div {:class "box column is-5 p-5"
               :style {:z-index 10}} (if content [content] "no content")]
        [:button.modal-close.is-large {:aria-label "close"
-                                      :on-click #(toggle-modal)}]])))
+                                      :on-click   #(toggle-modal)}]])))
 
 (defn make-modal
   "returns [^fn toggle-modal, ^fn close-modal, ^component <modal>]"
   []
-  (let [unique-id (gensym)
+  (let [unique-id    (gensym)
         select-modal (fn [] (.getElementById js/document unique-id))
         toggle-modal (fn [] (let [el        (select-modal)
                                   classList (.-classList el)]
                               (cond (.contains classList "is-active")
                                     (.remove classList "is-active")
                                     :else (.add classList "is-active"))))
-        close-modal (fn [] (let [el        (select-modal)
-                                 classList (.-classList el)]
-                             (when (.contains classList "is-active")
-                               (.remove classList "is-active"))))
-        <modal> (fn [content]
-                  [:div {:class "modal"
-                         :id unique-id}
-                   [:div {:class "modal-background"
-                          :on-click #(close-modal)}]
-                   [:div {:class "box column is-5 p-5"
-                          :style {:z-index 10}} (if content [content] "no content")]
-                   [:button.modal-close.is-large {:aria-label "close"
-                                                  :on-click #(toggle-modal)}]])]
+        close-modal  (fn [] (let [el        (select-modal)
+                                  classList (.-classList el)]
+                              (when (.contains classList "is-active")
+                                (.remove classList "is-active"))))
+        <modal>      (fn [content]
+                       [:div {:class "modal"
+                              :id    unique-id}
+                        [:div {:class    "modal-background"
+                               :on-click #(close-modal)}]
+                        [:div {:class "box column is-5 p-5"
+                               :style {:z-index 10}} (if content [content] "no content")]
+                        [:button.modal-close.is-large {:aria-label "close"
+                                                       :on-click   #(toggle-modal)}]])]
     ;; attatch the event listener:
-    (.addEventListener
-     js/document "keydown"
-     (fn [e]
-       (when (= (.-keyCode e) 27) (close-modal))))
+    (.addEventListener js/document "keydown"
+                       (fn [e]
+                         (when (= (.-keyCode e) 27) (close-modal))))
     ;; return the closures and component:
     [toggle-modal close-modal <modal>]))
