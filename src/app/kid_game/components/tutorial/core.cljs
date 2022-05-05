8becbb322cc3aa86767e3610da499b7111ff0254
niko <niko@niko.io>
2022-05-05 11:18:17 +0200

    Added skip button

--- src/app/kid_game/components/tutorial/core.cljs
+++ src/app/kid_game/components/tutorial/core.cljs
@@ -24,20 +24,25 @@
       [slides-buttons back! done! "Let me play already!"]])])
 
 (defn <slides> [cancel! finish! slides]
-(let [slide-index (r/atom 0)
-      next?       (fn [] (< @slide-index (dec (count slides))))
-      next!       (fn [] (reset! slide-index (inc @slide-index)))
-      back?       (fn [] (> @slide-index 0))
-      back!       (fn [] (reset! slide-index (dec @slide-index)))
-      get-slide   (fn [] (slides @slide-index))]
-  (fn []
-    [:div {:class "hero is-fullheight is-align-content-center is-justify-content-center has-background-light"}
-     [:div {:class "columns is-justify-content-center contain-section-width center-section has-background-white p-5 pt-6 br-2"}
-      [:div {:class "column is-12"}
-       [(get-slide)
-        (if (back?) back! cancel!)
-        (if (next?) next! finish!)]
-       ]]])))
+  (let [slide-index (r/atom 0)
+        next?       (fn [] (< @slide-index (dec (count slides))))
+        next!       (fn [] (reset! slide-index (inc @slide-index)))
+        back?       (fn [] (> @slide-index 0))
+        back!       (fn [] (reset! slide-index (dec @slide-index)))
+        get-slide   (fn [] (slides @slide-index))]
+    (fn []
+      [:div {:class "hero is-fullheight is-align-content-center is-justify-content-center has-background-light"}
+       [:div {:class "columns is-justify-content-center contain-section-width center-section has-background-white p-5 pt-6 br-2"}
+        [:div {:class "column is-12"}
+         [(get-slide)
+          (if (back?) back! cancel!)
+          (if (next?) next! finish!)]
+         ]
+
+        ]
+       [:div.columns.contain-section-width.center-section
+        [:a.has-text-grey.is-italic {:href "#" :on-click finish!} "skip"]
+        ]])))
 
 (defn <tutorial> [finish! back!]
-[<slides> back! finish! slides])
+  [<slides> back! finish! slides])
